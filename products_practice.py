ca8e0b77efae8e384d762f1bea63f305f5d644e5
Yuchang Fan <yuchangjimmyfan@gmail.com>
2021-08-10 23:21:12 +0800

    add products_simplify.py

---
+++ products_practice.py
@@ -0,0 +1,23 @@
+# products
+
+products = []
+while True:
+	name = input('請輸入商品名稱： ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格： ')
+
+	# 第一種寫法：
+	# p = []
+	# p.append(name)
+	# p.append(price)
+	# products.append(p)
+
+	# 第二種寫法：
+	# p = [name, price]
+	# products.append(p)
+
+	# 第三種寫法：
+	products.append([name, price])
+
+print(products)
