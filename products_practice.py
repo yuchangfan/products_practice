8dc7eab9f7f8bc2c7b73c771c1ce0d1769920893
Yuchang Fan <yuchangjimmyfan@gmail.com>
2021-08-12 22:39:05 +0800

    add products write

--- products_practice.py
+++ products_practice.py
@@ -24,3 +24,8 @@ print(products)
 
 for product in products:
 	print(product[0], '的價格是：', product[1])
+
+with open('products.csv' , 'w') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
