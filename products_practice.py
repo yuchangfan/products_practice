8a874172c96959741d3cf0ba8f75fc5c6e48aa86
Yuchang Fan <yuchangjimmyfan@gmail.com>
2021-08-12 23:03:36 +0800

    add read file

--- products_practice.py
+++ products_practice.py
@@ -1,6 +1,16 @@
 # products
+# 讀取檔案
 
 products = []
+with open('products.csv', 'r') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+
+# 使用者輸入新品項
+
 while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q':
@@ -22,8 +32,12 @@ while True:
 
 print(products)
 
+# 印出購買紀錄（商品 + 價格）
+
 for product in products:
 	print(product[0], '的價格是：', product[1])
+
+# 寫入檔案
 
 with open('products.csv' , 'w') as f:
 	f.write('商品,價格\n')
