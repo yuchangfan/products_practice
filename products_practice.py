4feb0ff32a08f73337f3a7c0741609c3bc4981a9
Yuchang Fan <yuchangjimmyfan@gmail.com>
2021-08-12 23:21:05 +0800

    add if file exist

--- products_practice.py
+++ products_practice.py
@@ -1,13 +1,20 @@
-# products
-# 讀取檔案
+# products_practice.py
 
+import os
 products = []
-with open('products.csv', 'r') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
+
+# 檢查檔案在不在
+if os.path.isfile('products.csv'):
+	# 讀取檔案
+	print('yeah! 找到檔案了！')
+	with open('products.csv', 'r') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+else:
+	print('找不到檔案......')
 
 # 使用者輸入新品項
 
@@ -33,12 +40,10 @@ while True:
 print(products)
 
 # 印出購買紀錄（商品 + 價格）
-
 for product in products:
 	print(product[0], '的價格是：', product[1])
 
 # 寫入檔案
-
 with open('products.csv' , 'w') as f:
 	f.write('商品,價格\n')
 	for p in products:
